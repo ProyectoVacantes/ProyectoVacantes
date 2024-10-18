a164c0ee798e56f4d23045dd1555da50f5fafddb
Miquel Moyano <miquel.moyano@gmail.com>
2024-10-18 10:47:36 +0200

    Mapa en la home

--- src/app_vacantes/lib/screens/home_screen.dart
+++ src/app_vacantes/lib/screens/home_screen.dart
@@ -1,19 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(color: Color(0xFF49BB88)),
-        child: const Center(
-          child: Text('Pantalla inicial'),
-        ),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: screenWidth,
+            height: screenHeight,
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(37.18817, -3.60667), //Coordenadas de inicio
+                zoom: 13.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: screenWidth * 0.1,
+                      height: screenWidth * 0.1,
+                      point: LatLng(37.18817, -3.60667),
+                      builder: (ctx) => Icon(
+                        Icons.location_on,
+                        color: Colors.red,
+                        size: screenWidth * 0.02, 
+                      ), //icono de ejemplo
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.02,
+            right: screenWidth * 0.02,
+            child: Text(
+              '© OpenStreetMap contributors',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 61, 61, 61),
+                fontSize: screenWidth * 0.008,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
