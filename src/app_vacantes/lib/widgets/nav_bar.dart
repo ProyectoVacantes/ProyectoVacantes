3919785feb30ad198a8f4cf595d55eb36c18f617
xenocrisis <akaxenonxss@gmail.com>
2024-10-21 11:25:54 +0200

    FV-89 | He arreglado el fallo de la navegación entre pantallas. Con este parche, cuando NAVBAR llama a HomeScreen en el child de la linea 40 recurriendo al array de _screens lo hace mediante el parrametro isCallingFromNavBar evitando que el mismo HomeScreen vuelva a llamar de nuevo NAVBAR causando un bucle. También he tenido que ajustar los indices de referencia de cada screen.

--- src/app_vacantes/lib/widgets/nav_bar.dart
+++ src/app_vacantes/lib/widgets/nav_bar.dart
@@ -1,55 +1,63 @@
 import 'package:app_vacantes/screens/filtro_screen.dart';
 import 'package:app_vacantes/screens/home_screen.dart';
 import 'package:app_vacantes/screens/legal_screen.dart';
-import 'package:app_vacantes/screens/leyenda_screen.dart';
-import 'package:app_vacantes/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeNavBar extends StatefulWidget {
-  const HomeNavBar({super.key});
+  final int callingScreen; // Se espera un índice para la pantalla inicial
+
+  const HomeNavBar({super.key, required this.callingScreen});
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<HomeNavBar> {
-  int _selectedIndex = 0;
+  late int _selectedIndex; // Usamos 'late' para inicializar después
 
-  static List<Widget> _widgetOptions = <Widget>[
+  static const List<Widget> _screens = <Widget>[
+    HomeScreen(isCallingFromNavBar: true),
     FiltroScreen(),
-    HomeScreen(),
-    LeyendaScreen(),
+    LegalScreen(),
   ];
-  void _selectedOptionInMyBottomNavigation(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.callingScreen; // Inicializamos el índice
   }
 
-  void onItemTap(int selectedItems) {}
+  void _selectedOptionInMyBottomNavigation(int index) {
+    setState(() {
+      _selectedIndex = index; // Actualizamos el índice seleccionado
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: _screens[_selectedIndex], // Mostramos la pantalla correspondiente
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: "Consulta"),
           BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label:
-                  "home"), //Al pulsar home entra en bucle y genera otra navbar
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: "Consulta",
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.help),
             label: "Ayuda",
-          )
+          ),
         ],
         currentIndex: _selectedIndex,
         unselectedItemColor: Colors.grey,
         selectedItemColor: Colors.black,
-        onTap: _selectedOptionInMyBottomNavigation,
+        onTap: _selectedOptionInMyBottomNavigation, // Manejador de selección
       ),
     );
   }
