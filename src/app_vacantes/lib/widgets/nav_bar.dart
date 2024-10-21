0550b03e23f4555a6e447067d94553879fd902e5
jmoreno999 <jmp0033@alu.medac.es>
2024-10-21 10:04:32 +0200

    Implementación de navegación entre pantallas hecha, a falta de corregir un error al pulsar home.

--- src/app_vacantes/lib/widgets/nav_bar.dart
+++ src/app_vacantes/lib/widgets/nav_bar.dart
@@ -1,46 +1,55 @@
+import 'package:app_vacantes/screens/filtro_screen.dart';
+import 'package:app_vacantes/screens/home_screen.dart';
 import 'package:app_vacantes/screens/legal_screen.dart';
+import 'package:app_vacantes/screens/leyenda_screen.dart';
 import 'package:app_vacantes/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeNavBar extends StatefulWidget{
+class HomeNavBar extends StatefulWidget {
   const HomeNavBar({super.key});
 
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<HomeNavBar>{
-  int _SelectedIndex=0;
+class _HomeState extends State<HomeNavBar> {
+  int _selectedIndex = 0;
 
-  static List<Widget> _widgedOptions= <Widget>[
-    Text("Index 1"),
-    SplashScreen(),
-    LegalScreen(),
+  static List<Widget> _widgetOptions = <Widget>[
+    FiltroScreen(),
+    HomeScreen(),
+    LeyendaScreen(),
   ];
-  void _selectedOptionInMyBottomNavigation(int index){
+  void _selectedOptionInMyBottomNavigation(int index) {
     setState(() {
-      _SelectedIndex = index;
+      _selectedIndex = index;
     });
   }
+
+  void onItemTap(int selectedItems) {}
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center
-      (child: _widgedOptions.elementAt(_SelectedIndex),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-    bottomNavigationBar: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home), label: "home"),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.list), label: "Consulta"),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.help), label: "Ayuda")
-      ],
-      currentIndex: _SelectedIndex,
-      unselectedItemColor: Colors.grey,
-      selectedItemColor: Colors.black,
-      onTap: _selectedOptionInMyBottomNavigation,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: "Consulta"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label:
+                  "home"), //Al pulsar home entra en bucle y genera otra navbar
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: "Ayuda",
+          )
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black,
+        onTap: _selectedOptionInMyBottomNavigation,
       ),
     );
   }
